4862f28d264945a6030d515fe373b787b297b5a0
Cao Xiaoqi <xqcao@thoughtworks.com>
2018-11-12 00:29:58 +0800

    Support scss in test

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './index.scss';
-import 'babel-polyfill';
+import '@babel/polyfill';
 import HelloPage from './pages/HelloPage';
 
 document.getElementById('app').append(new HelloPage().render("World"));
--- src/pages/HelloPage.js
+++ src/pages/HelloPage.js
@@ -1,8 +1,11 @@
+import styles from './HelloPage.scss';
+
 class HelloPage {
   constructor() {}
 
   render(message) {
     let dom = document.createElement('div');
+    dom.className = styles.hello;
     dom.append(`Hello: ${message}`);
     return dom;
   }
